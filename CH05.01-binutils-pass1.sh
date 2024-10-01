60399cc8caf7c8fa877f5c4651a87086a6771c3c
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-01 16:23:18 -0700

    toolchain work

---
+++ CH05.01-binutils-pass1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source versions.sh
+
+GLSOURCES="${LFS}/sources"
+
+function myfail {
+  echo "$1"
+  exit 1
+}
+
+
+if [ "`whoami`" != "lfs" ]; then
+  myfail "Must run this script as lfs user"
+fi  
+
+pushd $GLSOURCES > /dev/null 2>&1 || myfail "Failed to move to ${GLSOURCES}"
+
+tar -Jxf ${binutils_tarball}
+cd binutils-${binutils_version}
+
+mkdir build
+cd build
+
+../configure --prefix=${LFS}/tools \
+  --with-sysroot=${LFS} \
+  --target=${LFS_TGT} \
+  --disable-nls \
+  --enable-gprofng=no \
+  --disable-werror    \
+  --enable-new-dtags  \
+  --enable-default-hash-style=gnu
+
+make
+
+if [ $? -ne 0 ]; then
+  myfail "Failed building binutils pass 1"
+fi
+
+make install
+
+if [ $? -ne 0 ]; then
+  myfail "Failed installing binutils pass 1"
+fi
---
+++ CH05.02-gcc-pass1.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# FIXME - go and objc
+
+source versions.sh
+
+GLSOURCES="${LFS}/sources"
+
+function myfail {
+  echo "$1"
+  exit 1
+}
+
+
+if [ "`whoami`" != "lfs" ]; then
+  myfail "Must run this script as lfs user"
+fi  
+
+pushd $GLSOURCES > /dev/null 2>&1 || myfail "Failed to move to ${GLSOURCES}"
+
+tar -Jxf ${gcc_tarball}
+cd gcc-${gcc_version}
+
+tar -xf ../${mpfr_tarball}
+mv mpfr-${mpfr_version} mpfr
+tar -xf ../${gmp_tarball}
+mv gmp-${gmp_version} gmp
+tar -xf ../${mpc_tarball}
+mv mpc-${mpc_version} mpc
+
+mkdir build
+cd build
+
+../configure \
+  --target=${LFS_TGT} \
+  --prefix=${LFS}/tools \
+  --with-glibc-version=2.40 \
+  --with-sysroot=${LFS} \
+  --with-newlib \
+  --without-headers \
+  --enable-default-pie \
+  --enable-default-ssp \
+  --disable-nls \
+  --disable-shared \
+  --disable-multilib \
+  --disable-threads \
+  --disable-libatomic \
+  --disable-libgomp \
+  --disable-libquadmath \
+  --disable-libssp \
+  --disable-libvtv \
+  --disable-libstdcxx \
+  --enable-languages=c,c++
+
+
+make
+
+if [ $? -ne 0 ]; then
+  myfail "Failed building gcc pass 1"
+fi
+
+make install
+
+if [ $? -ne 0 ]; then
+  myfail "Failed installing gcc pass 1"
+fi
