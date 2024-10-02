ce904296471d23b5867010562cf80bc9e4023263
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-01 17:06:18 -0700

    missing case

--- CH05.02-gcc-pass1.sh
+++ CH05.02-gcc-pass1.sh
@@ -28,6 +28,13 @@ mv gmp-${gmp_version} gmp
 tar -xf ../${mpc_tarball}
 mv mpc-${mpc_version} mpc
 
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+  ;;
+esac
+
 mkdir build
 cd build
 
