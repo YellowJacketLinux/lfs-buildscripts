a00d1435df3fe103d69bdcf59b031cae801d44b3
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-03 12:17:25 -0700

    CH7 perl

---
+++ chrootscripts/CH07.05-perl.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source versions.sh
+
+GLSOURCES="/sources"
+
+pushd $GLSOURCES > /dev/null 2>&1 || myfail "Failed to move to ${GLSOURCES}"
+
+[ -d perl-${perl_version} ] && rm -rf perl-${perl_version}
+
+tar -Jxf ${perl_tarball}
+
+cd perl-${perl_version}
+
+vlib="/usr/lib/perl5/${perl_major_version}"
+
+sh Configure -des \
+  -D prefix=/usr \
+  -D vendorprefix=/usr \
+  -D useshrplib \
+  -D privlib=${vlib}/core_perl \
+  -D archlib=${vlib}/core_perl \
+  -D sitelib=${vlib}/site_perl \
+  -D sitearch=${vlib}/site_perl \
+  -D vendorlib=${vlib}/vendor_perl \
+  -D vendorarch=${vlib}/vendor_perl
+
+make
+
+if [ $? -ne 0 ]; then
+  myfail "Failed building perl"
+fi
+
+make install
+
+if [ $? -ne 0 ]; then
+  myfail "Failed installing perl"
+fi
