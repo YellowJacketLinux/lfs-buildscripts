c6e44ab8a66776fcb3b5308e8f7a54e387a18830
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-05 15:01:50 -0700

    file reorgananing

--- CH6Build/CH06-Master.sh
+++ CH6Build/CH06-Master.sh
@@ -2,57 +2,6 @@
 
 # compiles the toolchain components
 
-/bin/bash CH05.01-binutils-pass1.sh
-
-if [ $? -ne 0 ]; then
-  echo "failed CH05.01-binutils-pass1.sh"
-  exit 1
-else
-  echo "LFS Chapter 5 binutils Pass 1 complete"
-  sleep 3
-fi
-
-/bin/bash CH05.02-gcc-pass1.sh
-
-if [ $? -ne 0 ]; then
-  echo "failed CH05.02-gcc-pass1.sh"
-  exit 1
-else
-  echo "LFS Chapter 5 GCC Pass 1 complete"
-  # warms up the CPU
-  sleep 60
-fi
-
-/bin/bash CH05.03-linux-api-headers.sh
-
-if [ $? -ne 0 ]; then
-  echo "failed CH05.03-linux-api-headers.sh"
-  exit 1
-else
-  echo "LFS Chapter 5 Linux API Headers complete"
-  sleep 3
-fi
-
-/bin/bash CH05.04-glibc.sh
-
-if [ $? -ne 0 ]; then
-  echo "failed CH05.03-linux-api-headers.sh"
-  exit 1
-else
-  echo "LFS Chapter 5 Glibc 2.40 complete"
-  sleep 3
-fi
-
-/bin/bash CH05.05-libstdc++.sh
-
-if [ $? -ne 0 ]; then
-  echo "failed CH05.05-libstdc++.sh"
-  exit 1
-else
-  echo "LFS Chapter 5 Libstdc++ from GCC 14.2.0 complete"
-  sleep 3
-fi
-
 /bin/bash CH06.01-m4.sh
 
 if [ $? -ne 0 ]; then
