ac11a28022428b8d292b8af57f9eeeb8b7f44d14
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-06 19:19:58 -0700

    cleanup files

--- CH8Build/Master2.sh
+++ CH8Build/Master2.sh
@@ -468,6 +468,13 @@ else
   sleep 3
 fi
 
+#cleanup
+
+rm -rf /tmp/{*,.*}
+find /usr/lib /usr/libexec -name \*.la -delete
+find /usr -depth -name $(uname -m)-lfs-linux-gnu\* | xargs rm -rf
+userdel -r tester
+
 
 
 
