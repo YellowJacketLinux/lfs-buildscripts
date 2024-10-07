d0680c2fee561643503d7e1890742e037cb6d483
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-07 10:27:59 -0700

    Adjust end of script backup command

--- CH7Build/Master.sh
+++ CH7Build/Master.sh
@@ -66,16 +66,18 @@ rm -rf /usr/share/{info,man,doc}/*
 find /usr/{lib,libexec} -name \*.la -delete
 rm -rf /tools
 
-echo "leave the chroot environment and unmount virtual file systems."
-echo
-echo "execute the following commands:"
+echo "### leave the chroot environment and unmount virtual file systems:"
 echo
 echo "exit"
 echo "mountpoint -q ${LFS}/dev/shm && umount ${LFS}/dev/shm"
 echo "umount ${LFS}/dev/pts"
 echo "umount ${LFS}/{sys,proc,run,dev}"
 echo
+echo "### then execute the following commands to backup the tools build:"
+echo
 echo "cd ${LFS}"
+echo "rm -rf sources && mkdir sources"
+echo "chown lfs sources"
 echo "tar -cJpf ${HOME}/lfs-temp-tools-12.2-systemd.tar.xz ."
 
 exit 0
