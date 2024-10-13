5b07c352bd02599ba7bfbe21a93903e979b177c7
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-13 03:57:44 -0700

    update /dev/sda2 UUID after format

--- CH9Config/02-mountpoints.sh
+++ CH9Config/02-mountpoints.sh
@@ -15,7 +15,7 @@ if [ ! -f /USBFlash ]; then
 # UUID/File System                        mount-point    type     options          dump fsck
 #                                                                                    order
 
-UUID=ed56dd97-52b1-4efb-8f2c-b83dee11ad74 /              ext4     defaults,noatime    1 1
+UUID=fed06a3d-5504-44ae-8b7d-c7357e7bad10 /              ext4     defaults,noatime    1 1
 UUID=2322f8e6-1d3d-43ee-becf-67417a46e124 /boot          ext2     defaults            1 2
 #UUID=5f6d2023-bea1-48af-9486-c5afe0f7b9c7 /home          ext4     defaults            1 2
 UUID=a36492d9-de00-4c51-9649-cd5b5004f790 swap           swap     defaults            0 0
@@ -28,7 +28,7 @@ else
 #                                                                                    order
 
 UUID=add2119a-1a46-4c72-b212-863ee869ebd1 / ext4 defaults,noatime 1 1
-UUID=ed56dd97-52b1-4efb-8f2c-b83dee11ad74 /mnt/newlfs    ext4     defaults,noatime    1 2
+UUID=fed06a3d-5504-44ae-8b7d-c7357e7bad10 /mnt/newlfs    ext4     defaults,noatime    1 2
 UUID=2322f8e6-1d3d-43ee-becf-67417a46e124 /mnt/newlfs/boot ext2   defaults,noauto     1 2
 UUID=a36492d9-de00-4c51-9649-cd5b5004f790 swap           swap     defaults            0 0
 EOF
